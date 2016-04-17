811c55d153ff8d13c4ea97a1eb1407047b3813e8
ocha <ochaochaocha3@gmail.com>
2016-04-17 13:00:18 +0900

    DodontoF::Logger にログ記録を委譲する

--- src_ruby/loggingFunction.rb
+++ src_ruby/loggingFunction.rb
@@ -1,39 +1,18 @@
 #--*-coding:utf-8-*--
 
+require 'dodontof/logger'
+
 def initLog
-  return unless( $log.nil? )
-  
-  $log = Logger.new($logFileName, $logFileMaxCount, $logFileMaxSize)
-  $log.level = ( $debug ? Logger::DEBUG : Logger::ERROR )
-  $log.level = Logger::FATAL if( $isModRuby )
-end
-
-
-def getLogMessageProc(obj, *options)
-  message = obj.inspect.tosjis
-  if( obj.instance_of?(String) )
-    message = obj
-  end
-  return message = "#{options.join(',')}:#{message}"
+  # ロガーのインスタンスが存在しなければ自動で生成され準備が行われる
+  DodontoF::Logger.instance
 end
 
 def logging(obj, *options)
-  return unless( $debug )
-  
-  $log.debug() do 
-    getLogMessageProc(obj, *options)
-  end
+  logger = DodontoF::Logger.instance
+  logger.debug(obj, *options)
 end
 
 def loggingForce(obj, *options)
-  $log.error() do 
-    getLogMessageProc(obj, *options)
-  end
-end
-
-def loggingTest(*args)
-end
-
-def debug(obj1, *obj2)
-  logging(obj1, *obj2)
+  logger = DodontoF::Logger.instance
+  logger.error(obj, *options)
 end
