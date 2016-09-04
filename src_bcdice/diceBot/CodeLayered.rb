2d713cd796bca881bce6c0816af40341da547e59
erina-alx <riena.alx@gmail.com>
2016-09-04 12:37:13 +0900

    コード：レイヤード : 判定の補正を +- 両方に対応＆三項以上に対応

--- src_bcdice/diceBot/CodeLayered.rb
+++ src_bcdice/diceBot/CodeLayered.rb
@@ -17,17 +17,19 @@ class CodeLayered < DiceBot
   end
   
   def prefixs
-     ['CL\d+(\+\d+)?(>=\d+)?']
+     ['CL\d+([\+\-]\d+)*(>=\d+)?']
   end
   
   def getHelpMessage
     return <<INFO_MESSAGE_TEXT
 CLx
 CLx+y
+CLx+y-v
 CLx>=z
 CLx+y>=z
+CLx+y-v>=z
 　x = 能力値
-　y = 技能レベル
+　y, v = 技能レベル、特技などによるダイス数の増減
 　z = 難易度
 INFO_MESSAGE_TEXT
   end
@@ -35,26 +37,59 @@ INFO_MESSAGE_TEXT
   def rollDiceCommand(command)
     
     case command
-      when /^CL(\d+)(\+(\d+))?\>\=(\d+)(\s+|$)/i
-        return check_cl($1.to_i, $3.to_i, $4.to_i)
-      when /^CL(\d+)(\+(\d+))?(\s+|$)/i
-        return check_cl($1.to_i, $3.to_i)
+      when /^CL(\d+)([+\-](\d+))*\>\=(\d+)(\s+|$)/i
+        ability = $1.to_i
+        difficulty = command.scan(/\>\=(\d+)/)[0][0].to_i
+        matched = command.split(' ')[0].scan(/[+\-]\d+/)
+        members = []
+        
+        unless matched.nil? then
+          members = matched.map do |x| member_to_i(x) end
+        end
+        
+        return check_cl(ability, members, difficulty)
+
+        return check_cl($1.to_i, [$3.to_i], $4.to_i)
+      when /^CL(\d+)([+\-](\d+))*(\s+|$)/i
+        ability = $1.to_i
+        matched = command.split(' ')[0].scan(/[+\-]\d+/)
+        members = []
+        
+        unless matched.nil? then
+          members = matched.map do |x| member_to_i(x) end
+        end
+        
+        return check_cl(ability, members)
     end
     
     return nil
   end
   
-  def check_cl(ability, additinal = 0, difficulty = -1, border = 6)
+  def member_to_i(m)
+    if m[0] == '+' then
+      m.slice(1..-1).to_i
+    else
+      m.to_i
+    end
+  end
+  
+  def check_cl(ability, additinals = [], difficulty = -1, border = 6)
     dices = []
     
-    (ability + additinal).times do
+    number_of_dice = ability
+    
+    unless additinals.empty? then
+      number_of_dice += additinals.inject(:+)
+    end
+    
+    number_of_dice.times do
       dice, = roll(1, 10)
       dices << dice
     end
     
     result = dices.count do |x| x <= border end
     
-    text = "#{ability + additinal}D#{10} |> [#{dices.join ","}] ≦ #{border}"
+    text = "#{number_of_dice}D#{10} |> [#{dices.join ","}] ≦ #{border}"
     
     if result > 0 then
       text += " |> #{result}"
