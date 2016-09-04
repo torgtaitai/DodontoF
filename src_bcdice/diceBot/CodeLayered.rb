b2047cd20d5848d092c8e4420490037afb0ce855
erina-alx <riena.alx@gmail.com>
2016-09-04 11:51:03 +0900

    コード：レイヤード : 難易度を指定できるようにして、その場合に成否を出力するようにした

--- src_bcdice/diceBot/CodeLayered.rb
+++ src_bcdice/diceBot/CodeLayered.rb
@@ -17,23 +17,30 @@ class CodeLayered < DiceBot
   end
   
   def prefixs
-     ['CL\d+(\+\d+)?']
+     ['CL\d+(\+\d+)?(>=\d+)?']
   end
   
   def getHelpMessage
     return <<INFO_MESSAGE_TEXT
 CLx
 CLx+y
+CLx>=z
+CLx+y>=z
 　x = 能力値
 　y = 技能レベル
+　z = 難易度
 INFO_MESSAGE_TEXT
   end
   
   def rollDiceCommand(command)
     
     case command
+      when /^CL(\d+)(\+(\d+))?\>\=(\d+)(\s+|$)/i
+        return check_cl($1.to_i, $3.to_i, $4.to_i)
       when /^CL(\d+)(\+(\d+))?(\s+|$)/i
         return check_cl($1.to_i, $3.to_i)
+      when /^CL(\d+)\>\=(\d+)(\s+|$)/i
+        return check_cl($1.to_i, 0, $2.to_i)
       when /^CL(\d+)(\s+|$)/i
         return check_cl($1.to_i)
     end
@@ -41,7 +48,7 @@ INFO_MESSAGE_TEXT
     return nil
   end
   
-  def check_cl(ability, additinal = 0, border = 6)
+  def check_cl(ability, additinal = 0, difficulty = -1, border = 6)
     dices = []
     
     (ability + additinal).times do
@@ -57,11 +64,22 @@ INFO_MESSAGE_TEXT
       text += " |> #{result}"
       
       critical_bonus = dices.count do |x| x == 1 end
+      result += critical_bonus
+      
       if critical_bonus >= 1 then
-        text += " |> Critical!(#{critical_bonus}) |> #{result + critical_bonus}"
+        text += " |> Critical!(#{critical_bonus}) |> #{result}"
       end
     else
       text += " |> ファンブル"
+      result = -1
+    end
+    
+    if difficulty >= 0 then
+      if result >= difficulty then
+        text += "（成功）"
+      else
+        text += "（失敗）"
+      end
     end
     
     return text
